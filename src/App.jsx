6f3375b5e95b265ed4fc69fdb0263f785716b80e
Koshal765 <koshalpothare@gamil.com>
2026-06-20 19:45:49 +0530

    save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import HomePage from "./Pages/Home";
 import Navbar from "./components/Navbar";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
-<<<<<<< HEAD
+
 import LaundryService from "./components/LaundryService";
 import DryCleaning from "./components/DryCleaning";
 import CurtainCleaning from "./components/CurtainCleaning";
@@ -15,14 +15,14 @@ import CarpetCleaning from "./components/CarpetCleaning";
 import Ironing from "./components/Ironing";
 
 
-=======
+
  
 import AdminDashboard from "./components/Admin/AdminDashboard";  
  
 import TermsConndition from "./Pages/TermsCondition";
 import FAQ from "./Pages/FAQ";
  
->>>>>>> 25b61bb0030bf608d5cd9b0b8f869bab85a15fc5
+
 
 function App() {
   const location = useLocation();
@@ -37,21 +37,21 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services" element={<Services />} />
-<<<<<<< HEAD
+
          <Route path="/services/laundry-service" element={<LaundryService />} />
          <Route path="/services/dry-cleaning" element={<DryCleaning />} />
          <Route path="/services/shoe-cleaning" element={<ShoeCleaning />} />
          <Route path="/services/ironing" element={<Ironing />} />
          <Route path="/services/carpet-cleaning" element={<CarpetCleaning />} />
          <Route path="/services/curtain-cleaning" element={<CurtainCleaning />} />
-=======
+
  
         <Route path="/dashboard" element={<AdminDashboard />} />  
  
         <Route path="/TermsCondition" element={<TermsConndition />} />
         <Route path="/FAQ" element={<FAQ />}/>
  
->>>>>>> 25b61bb0030bf608d5cd9b0b8f869bab85a15fc5
+
       </Routes>
 
       {!isDashboard && <Footer />}
