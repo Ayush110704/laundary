7459660e9c9a6260991d98cd777097cbdf833fd3
Ayush sengar <aayussengar.work@gmail.com>
2026-07-18 22:07:28 +0530

    app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -108,7 +108,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin-register" element={<AdminRegister />} />
         <Route
           path="/reset-password/:token"
